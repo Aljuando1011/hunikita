547a21447b34ca60402ee700a2860e42e9518c28
Web11Pro <web11prodafternoon@gmail.com>
2023-11-17 20:46:38 +0700

    add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Card from "./components/Card";
 import Carousel from "./components/Carousel";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -5,9 +6,19 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-    <Navbar />
+    {/* <Navbar />
     <Carousel />
-    <Footer />
+    <Footer /> */}
+    <div className="flex gap-5">
+    <Card />
+    <Card />
+    <Card />
+    </div>
+    <div className="flex gap-5">
+    <Card />
+    <Card />
+    <Card />
+    </div>
     </>
   );
 }
