1863ea2beb21d81077a329a195a285ad028c5ebc
RezaAdityaSaputra <rezaadityasaputra50@gmail.com>
2023-11-23 01:36:41 +0700

    kontrakan

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import Login from "./Pages/autentikasi/Login";
 import Register from "./Pages/autentikasi/Register"
 import Listkosan from "./Pages/kosan/Listkosan";
+import Detailkosan from "./Pages/kosan/Detailkosan";
 import DropdownHover from "./components/penyewa/Dropdown";
 import NavPenyewa from "./components/penyewa/NavPenyewa";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Beranda from "./Pages/beranda/Beranda"
 import TentangKami from "./Pages/tentang-kami/TentangKami"
 
+
+
+// function App() {
+//   return (
+//     <>
+//   <Listkosan/>
+//     </>
+//   );
+// }
+
+// export default App;
+
 function App() {
   return (
     <>
     <Router>
       <Routes>
-        <Route path="/" element={<Beranda />} />
-        <Route path="/tentangkami" element={<TentangKami />} />
+        <Route path="/" element={<Detailkosan />} />
+        {/* <Route path="/tentangkami" element={<TentangKami />} /> */}
       </Routes>
     </Router>
     </>
