59ee6629dc76a3f4a14aa3daddbed36dccb7f9bd
rafialbarkurniawan <rafialbar81@gmai.com>
2023-11-22 10:11:45 +0700

    menambahkan halaman list-kontrakan

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import Card from "./components/Card";
 import Carousel from "./components/beranda/Carousel";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ListKontrakan from "./kontrakan/list-kontrakan/ListKontrakan";
 
 function App() {
   return (
     <>
-    
+      <Router>
+        <Routes>
+          <Route path="/list-kontrakan" element={<ListKontrakan />} />
+        </Routes>
+      </Router>
     </>
   );
 }
