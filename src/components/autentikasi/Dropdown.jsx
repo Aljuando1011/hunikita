8aaf20a59245e212211ea69fdd18711316b50cb2
Web11Pro <web11prodafternoon@gmail.com>
2023-12-03 19:30:13 +0700

    modified dropdown component

--- src/components/autentikasi/Dropdown.jsx
+++ src/components/autentikasi/Dropdown.jsx
@@ -15,16 +15,16 @@ const Dropdown = () => {
   return (
     <div className="flex item-start">
       <div className="w-full h-full relative inline-flex">
-        <button
+        <div
           type="text"
           className="w-full h-full bg-gray-200 border focus:border-blue-500 focus:bg-white focus:outline-none rounded-lg text-left"
           required
         >
           {selectedOption || "Pilih role"}
-        </button>
+        </div>
 
         <div className="relative inline-flex">
-          <button
+          <div
             type="text"
             onClick={toggling}
             className="bg-white border focus:border-blue-500 focus:bg-white focus:outline-none rounded-lg"
@@ -35,19 +35,19 @@ const Dropdown = () => {
               strokeLinejoin="round"
               d="M19 9l-7 7-7-7"
             ></path>
-          </button>
+          </div>
         </div>
         {isOpen && (
           <div className="absolute top-7 right-12 z-9 mt-4 min-w-[200px] origin-top-right rounded-md border border-black bg-white shadow-lg">
             {options.map((option) => (
               <div>
-                <button
-                  type="button"
+                <div
+                  type="div"
                   onClick={onOptionsClicked(option)}
                   key={Math.random()}
                 >
                   {option}
-                </button>
+                </div>
               </div>
             ))}
           </div>
